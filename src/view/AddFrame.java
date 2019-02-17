5ddacef3c7d9e504879477a01b1189f6e9defe58
Laura <thompson.laura@protonmail.com>
2019-02-17 05:44:22 -0500

    changes made before moving to one window
    maybe useful to move over to main window file for certain functions

--- src/view/AddFrame.java
+++ src/view/AddFrame.java
@@ -2,19 +2,25 @@ package view;
 
 import javax.swing.*;
 
+import controller.Expense;
+import controller.ExpenseData;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.ListIterator;
 
-public class SecondFrame {
+public class AddFrame {
+	
 	private JFrame addExp = new JFrame("Add an expense");
 	private JTextField textField;
 	private JTextField textField_1;
 	
-	public SecondFrame() {
+	public AddFrame() {
 	
-		addExp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//addExp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		addExp.setSize(300,300);
 		addExp.getContentPane().setLayout(null);
 		
@@ -67,12 +73,30 @@ public class SecondFrame {
 
 			
 		});
-			
+		//submit button
 		JButton btnSubmit = new JButton("Add Item");
 		btnSubmit.setBounds(20, 206, 117, 29);
 		addExp.getContentPane().add(btnSubmit);
 		addExp.setVisible(true);
-	
 		
-	}	
-}
+		//making an arrayList to check functionality of add/delete/edit
+		ArrayList<String> expensesTest = new ArrayList<String>();
+		
+		//takes text from Expense Name text box and prints it out when submit button is pushed
+		btnSubmit.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				String expenseName = textField.getText();
+				//System.out.println(expenseName);
+				expensesTest.add(expenseName);
+		
+			//checking contents of arrayList
+			ListIterator<String> ltr = expensesTest.listIterator();
+			   	while(ltr.hasNext()){
+			    System.out.println(ltr.next()); 
+			};
+			
+	
+		};
+		
+	});	
+}}
